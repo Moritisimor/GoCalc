f7a54beea5cb3f22bc10ac26fe35f454b7959f4a
Moritisimor <devMoritisimor@proton.me>
2025-07-26 15:39:49 +0200

    Fixed rad2deg and deg2rad formula switchup

--- GoCalc/lessBasicArithm.go
+++ GoCalc/lessBasicArithm.go
@@ -11,11 +11,11 @@ func sqroot(x float64) float64 {
 }
 
 func deg2rad(x float64) float64 {
-	return x * math.Pi / 180
+	return x / math.Pi * 180
 }
 
 func rad2deg(x float64) float64 {
-	return x / 180 * math.Pi
+	return x * 180 / math.Pi
 }
 
 func degsine(x float64) float64 {
